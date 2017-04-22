228b1e58ce4e8ed6eb53542aaf903405d32e1721
Julian Coleman <julcol03@gmail.com>
2017-04-22 12:18:54 -0700

    remove .env

--- Knexfile.js
+++ Knexfile.js
@@ -9,7 +9,7 @@ numberTypes.forEach(type => pg.types.setTypeParser(type, "text", parseFloat));
 module.exports = {
   client: "postgresql",
   connection: {
-    host: process.env.DATABSE_HOST,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_SKEY,
     database: process.env.DB_NAME,
