d101849dafa14631c485e1adf9516e35a12f0a62
Julian Coleman <julcol03@gmail.com>
2017-05-30 16:01:02 -0700

    add ability to patch a camp

---
+++ controllers/camps/put.js
@@ -0,0 +1,24 @@
+const { respondCustomError } = appRequire("helpers/responses");
+const Service = appRequire("lib/camps/service");
+const Validator = appRequire("lib/camps/put/validator");
+
+const CampNotFoundError = appRequire("lib/camps/errors/camp_not_found_error");
+
+const put = ({ payload, params }, reply) => {
+  const { campId } = params;
+
+  Service.updateCamp(campId, payload)
+    .then(camp => reply(camp))
+    .catch(CampNotFoundError, respondCustomError(reply));
+};
+
+module.exports = {
+  method: "PUT",
+  path: "/api/v1/camps/{campId}",
+  handler: put,
+  config: {
+    validate: {
+      payload: Validator,
+    },
+  },
+};
