e73521432c1fc131bd3285d3abf80478068bd69c
Julian <julcol03@gmail.com>
2017-06-02 15:49:05 -0700

    clean up itineraries

--- controllers/itineraries/get.js
+++ controllers/itineraries/get.js
@@ -3,7 +3,6 @@ const Service = appRequire("lib/itineraries/service");
 
 const ItineraryNotFoundError = appRequire("lib/itineraries/errors/itinerary_not_found_error");
 
-
 const getItinerary = ({ params }, reply) => {
   const { itineraryId } = params;
 
@@ -15,21 +14,8 @@ const getItinerary = ({ params }, reply) => {
     );
 };
 
-const getItineraries = (request, reply) => {
-  Service.getItineraries()
-    .then(itineraries => reply(itineraries))
-    .catch(respondCustomError(reply));
+module.exports = {
+  method: "GET",
+  path: "/api/v1/itineraries/{itineraryId}",
+  handler: getItinerary,
 };
-
-module.exports = [
-  {
-    method: "GET",
-    path: "/api/v1/itineraries",
-    handler: getItineraries,
-  },
-  {
-    method: "GET",
-    path: "/api/v1/itineraries/{itineraryId}",
-    handler: getItinerary,
-  },
-];
