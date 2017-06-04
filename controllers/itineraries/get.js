cfefb64eb8aebc191346a6401bc2c78fb59d4f2f
Julian <julcol03@gmail.com>
2017-06-04 10:21:59 -0700

    should only return itineraries for camps where a user is a Campus Leader

--- controllers/itineraries/get.js
+++ controllers/itineraries/get.js
@@ -3,6 +3,11 @@ const Service = appRequire("lib/itineraries/service");
 
 const ItineraryNotFoundError = appRequire("lib/itineraries/errors/itinerary_not_found_error");
 
+const getItineraries = ({ user }, reply) => {
+  Service.getItineraries(user)
+    .then(itineraries => reply(itineraries));
+};
+
 const getItinerary = ({ params }, reply) => {
   const { itineraryId } = params;
 
@@ -14,8 +19,15 @@ const getItinerary = ({ params }, reply) => {
     );
 };
 
-module.exports = {
-  method: "GET",
-  path: "/api/v1/itineraries/{itineraryId}",
-  handler: getItinerary,
-};
+module.exports = [
+  {
+    method: "GET",
+    path: "/api/v1/itineraries",
+    handler: getItineraries,
+  },
+  {
+    method: "GET",
+    path: "/api/v1/itineraries/{itineraryId}",
+    handler: getItinerary,
+  },
+];
--- models/itinerary.js
+++ models/itinerary.js
@@ -6,6 +6,25 @@ const ItineraryNotFoundError = appRequire("lib/itineraries/errors/itinerary_not_
 const itinerary = BaseModel.extend({
   tableName: "itinerary",
   customError: ItineraryNotFoundError,
+  accessibleCampusLeaderItineraries(user) {
+    const accessibleItineraryIds = Bookshelf.knex
+      .select("itinerary.id")
+      .from("itinerary")
+      .innerJoin("camp", "itinerary.camp_id", "camp.id")
+      .innerJoin("role_camp", "camp.id", "role_camp.camp_id")
+      .innerJoin("role", "role_camp.role_id", "role.id")
+      .innerJoin("user", "role.user_id", "user.id")
+      .where({
+        "user.id": user.get("id"),
+        "role.name": "Campus Leader",
+        "role.deleted_at": null,
+        "role_camp.deleted_at": null,
+        "camp.deleted_at": null,
+        "itinerary.deleted_at": null,
+      });
+
+    return this.query(qb => qb.whereIn("id", accessibleItineraryIds));
+  },
   camp() {
     return this.belongsTo("Camp");
   },
